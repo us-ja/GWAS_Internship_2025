014807c654900f2d632669c2f5ff0b2a2cb873e9
Janis Waser <waserj@usi.ch>
2025-07-23 15:55:24 +0200

    make it modulo 4

--- Changedatasets/changedataset.py
+++ Changedatasets/changedataset.py
@@ -5,7 +5,7 @@ from my_utils import *
 seed=10
 random.seed(seed)
 def givepers(l:int):
-    return list(range(l*25, (l+1)*25))
+    return list(range(l%4*25, (l%4+1)*25))
 
 
 comb_res=(combine_build_up(200, "HapMap",add_comm="25per_l", seed=seed, change_pers_func=givepers, allow_unknowns=20))
