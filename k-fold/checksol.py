fe9367d466a491847189b741147960e80dba5a58
Janis Waser <waserj@usi.ch>
2025-07-25 14:45:32 +0200

    analysis for newest results

--- k-fold/checksol.py
+++ k-fold/checksol.py
@@ -3,9 +3,11 @@ sys.path.append('..')
 import random
 from my_utils import *
 seed=10
-
-
-compare("given9foldbound_enf200_4/result55.txt")         
+compare("given9foldbound_enf200_4/result55.txt", ) 
+compare("given9fold_s2bound_enf200_4/result53.txt", )  
+compare("given9fold_s3bound_enf200_4/result54.txt", )  
+compare("given9fold_s96bound_enf200_4/result53.txt", )  
+compare("given9fold_s286bound_enf200_4/result52.txt", )         
                
         
          
--- test2.py
+++ test2.py
@@ -1,14 +1,24 @@
+# with open("HapMap.ped")as file:
+#     lines=file.readlines()
+#     last=0
+#     count=0
+#     for line in lines:
+        
+#         var=line.split()
+#         print((var[1]))
+#         if "5" in var[1] or "9" in var[1] :
+#             count+=1
+# print(count)
+
+
+
 with open("HapMap.ped")as file:
     lines=file.readlines()
     last=0
     count=0
     for line in lines:
         
-        var=line.split()
-        print((var[1]))
-        if "5" in var[1] or "9" in var[1] :
-            count+=1
-print(count)
-
-
+        var=line.split('\t')
+        print((var[-10:]))
         
+print(count)
