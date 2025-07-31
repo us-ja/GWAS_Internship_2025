60c597f9d273f81045a468b0232a90de10e3126e
Janis Waser <waserj@usi.ch>
2025-07-31 10:55:36 +0200

    change specific to HapMap to be more general

--- BloodLipid/firsttest.py
+++ BloodLipid/firsttest.py
@@ -3,20 +3,21 @@ sys.path.append('..')
 import random
 from my_utils import *
 res=[]
-def givepers(l:int, sel_pers:list=[]):
+def givepers(l:int, sel_pers:list=[], fileprefix="blood_lipid"):
+    k=get_total_pers(fileprefix=fileprefix)
     if sel_pers==[]:
-        return list(range(l%4*25, (l%4+1)*25))
+        return list(range(l%4*int(0.225*k), (l%4+1)*int(0.225*k)))
     else:
-        return sel_pers[l%4*25:(l%4+1)*25]
-print(get_total_snp("blood_lipid"))
+        return sel_pers[l%4*int(0.225*k):(l%4+1)*int(0.225*k)]
+print(get_total_snp("blood_lipid"), get_total_pers("blood_lipid"))
 seed=100
 print("start with seed", seed, "at", curr_time())
 random.seed(seed)
-sel_pers=(list(range(109)))
+sel_pers=(list(range(get_total_pers("blood_lipid"))))
 random.shuffle(sel_pers)
 print(sel_pers)
 comm="25_s"+str(seed)
-res.append(combine_build_up(200, "blood_lipid",add_comm=comm, seed=seed, sel_pers=sel_pers,change_pers_func=givepers, checkdoubles=False,shuffle_in_level=True, total_snp=1000))
+res.append(combine_build_up(300, "blood_lipid",add_comm=comm, seed=seed, sel_pers=sel_pers,change_pers_func=givepers, checkdoubles=False,shuffle_in_level=True, total_snp=1000))
 
 for e in res:
     print("\n Analysis of ",e)
