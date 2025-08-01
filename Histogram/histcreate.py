b645a6658dace62dc1120315f6fac5131003d543
Janis Waser <waserj@usi.ch>
2025-08-01 10:44:57 +0200

    add amount of files to label

--- Histogram/histcreate.py
+++ Histogram/histcreate.py
@@ -10,10 +10,8 @@ import math
 
 n=get_total_snp("../HapMap")
 
-shuffle=[0]*n
-pyramid=[0]*n
-files=get_files("../k-fold", "given", "result", ["Old", "l_", "Shuffle_Pheno", "Showcase"])
-print(files)
+
+
 def get_res(files):
     heights=[0]*n
     for e in files:
@@ -25,22 +23,18 @@ def get_res(files):
             ele[0] =ele[0][1:]
             ele[-1]=ele[-1][:-2]
             res.update( map(int, map(abs,map(float, ele))))
-        print(res)
+        
         for f in res:
             heights[f]+=1
         file.close()
     return heights
-heights=get_res(files)
+
 # print(heights)
-files=get_files("../Shuffle_Pheno", "given", "result", not_in_subdir=["l_"])
-print(files)
-shuffle=get_res(files)
 
-files=get_files("../Changedatasets", "given", "result", )
-print(files)
-pyramid=get_res(files)
 
-def mybars(heights, color, label, bottom=None):
+def mybars(color, label, dir:str=".", in_subdir:str=None, in_file:str=None, not_in_subdir:list=None, not_in_file:list=None,  bottom=None, print_f=False, pres_h=False):
+    files=get_files(dir, in_subdir, in_file, not_in_subdir, not_in_file, print_f=print_f)
+    heights=get_res(files)
     first=True
     
         
@@ -51,16 +45,17 @@ def mybars(heights, color, label, bottom=None):
             else:
                 ymin=bottom[i]
             if first:
-                plt.vlines(x=i, ymin=ymin, ymax=ymin+heights[i], colors=color, label=label, )
+                plt.vlines(x=i, ymin=ymin, ymax=ymin+heights[i], colors=color, label=label+" ("+str(len(files))+")", )
                 first=False
             else:
                 plt.vlines(x=i, ymin=ymin, ymax=heights[i]+ymin, colors=color)
             if ymin+heights[i]>2:
                 plt.text(x=i, y=heights[i]+0.1+ymin, s=i, fontsize=(heights[i]+ymin)*0.7+1, color=color)
-                
-mybars(heights, "blue", "k-fold")
-mybars(shuffle, "red", "Phenotype Shuffle")
-mybars(pyramid, "green", "Selection", heights)
+    if pres_h:
+        return heights
+k_fold=mybars( "blue", "k-fold", "../k-fold", "given", "result", ["Old", "l_", "Shuffle_Pheno", "Showcase"], pres_h=True)
+mybars("red", "Phenotype Shuffle", "../Shuffle_Pheno", "given", "result", not_in_subdir=["l_"], print_f=True)
+mybars( "green", "Selection", "../Changedatasets", "given", "result", bottom=k_fold)
 plt.ticklabel_format(useOffset=False, style='plain')
 plt.legend(loc="upper left")
 
