3676e8e5c7cd0b0ccc6093407901cb8d2f7d7822
Janis Waser <waserj@usi.ch>
2025-08-04 17:45:03 +0200

    create graph only for grouping scheme

--- Changedatasets/selbox.py
+++ Changedatasets/selbox.py
@@ -8,26 +8,26 @@ seed=10
 
 print("Started at", curr_time())              
 
-sh=get_shares(get_files(".", "s1", "res", ["bound"]),accept_lim=True, )
+sh=get_shares(get_files(".", "s1", "res", ["bound"]),accept_lim=False, )
 
-bound=get_shares(get_files(".", "s1", "res", ["shuffle"]),accept_lim=True)
+bound=get_shares(get_files(".", "s1", "res", ["shuffle"]),accept_lim=False)
 
-alt_b=get_shares(get_files(".", "alter", "res",),accept_lim=True)
+alt_b=get_shares(get_files(".", "alter", "res",),accept_lim=False)
 
-# alt_s=get_shares(get_files(".", "alter", "res",["bound"]))
+alt_s=get_shares(get_files(".", "alter", "res",["bound"]))
 
-kfold_b= get_shares(get_files("../k-fold", "bound", "res",) )
-kfold_s= get_shares(get_files("../k-fold", "shuffle", "res",))
+# kfold_b= get_shares(get_files("../k-fold", "bound", "res",) )
+# kfold_s= get_shares(get_files("../k-fold", "shuffle", "res",))
 
 plt.figure(figsize=(10,5))
-plt.title('Out of sample prediction accuracy')
+plt.title('Grouping Scheme: not in last level prediction accuracy')
 # print(sh)
 # print( np.quantile(sh, np.array([0.00, 0.25, 0.50, 0.75, 1.00]),method='closest_observation'))
 # print(np.quantile(bound, 0.75), np.mean(bound))
 # print( np.quantile(bound, np.array([0.00, 0.25, 0.50, 0.75, 1.00])))
 # print(np.quantile(bound, 0.25), np.mean(bound))
-data=[sh, bound, alt_b,kfold_s,kfold_b ]
-labels=["Shuffle 25\n"+str(len(sh)), "Ordered 25\n"+str(len(bound)), "Alternate\n"+str(len(alt_b)),"Pyramid shuffle \n"+str(len(kfold_s)),"Pyramid bound\n"+str(len(kfold_b))] 
+data=[sh, bound, alt_b,alt_s]#kfold_s,kfold_b ]
+labels=["Shuffle 25\n"+str(len(sh)), "Ordered 25\n"+str(len(bound)), "Alternate Bound\n"+str(len(alt_b)),"Alternate Shuffle\n"+str(len(alt_s))]#"Pyramid shuffle \n"+str(len(kfold_s)),"Pyramid bound\n"+str(len(kfold_b))] 
 plt.boxplot(data,tick_labels=labels, medianprops=dict(color='blue'))
 
 # plt.violinplot([sh, bound, alt_b,kfold_s,kfold_b ],showmeans=False, showmedians=True , )
@@ -40,7 +40,7 @@ for i in range(len(data)):
 
 plt.axhline(54/1.07, color='red', linestyle=':')
 plt.subplots_adjust(left=None, bottom=0.14, wspace=None)
-plt.savefig('../Documentation/accuracyplot.eps', format='eps')
+plt.savefig('../Documentation/selbox.eps', format='eps')
 print("finished all at", curr_time())
 plt.show()
 
