ddc6ef6edb6df949c3acc0962d8f909ecedb38bd
Janis Waser <waserj@usi.ch>
2025-08-05 15:38:35 +0200

    force the making of a product

---
+++ Productstest/shuf25.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('..')
+import random
+from my_utils import *
+res=[]
+fileprefix="HapMap"
+
+choosen=34
+hap=open("HapMap.ped")
+l=hap.readlines()
+hap.close()
+ids=[]
+ref=l[1].split("\t")[choosen]
+for line in l:
+    var=line.split("\t")
+    if int(var[5])==2 or (var[choosen])==ref:
+        ids.append(var[1])  
+
+
+
+for seed in range(200, 205):
+    pyramid25(fileprefix, seed, 200, plines=l, change_pheno=(lambda x: 1 if x in ids else 0), )
