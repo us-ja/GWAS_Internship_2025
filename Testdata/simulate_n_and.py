d00151baeee25c7198bff92a439569d526741398
Janis Waser <waserj@usi.ch>
2025-08-08 16:27:02 +0200

    changes to run it on server

--- Testdata/simulate_n_and.py
+++ Testdata/simulate_n_and.py
@@ -8,12 +8,12 @@ total_pers=309
 seed=300
 
 
-for prod in range(1,10):
+for prod in range(1,20):
     def_ph=list(range(1,prod+1))
     fileprefix="testdata_big"+str(len(def_ph))
     createdata(fileprefix, total_snp,total_pers,seed, def_ph)
     print("created data", curr_time())
-    predic_acc=grouping(fileprefix, seed=prod, g_size=200, total_snp=1000, deletelog=True, given_share=1, controlshare=0)
+    predic_acc=grouping(fileprefix, seed=prod, g_size=200, total_snp=1000,  controlshare=0.1)
     if predic_acc[0]==None or int(predic_acc[0][0]) != 100 or int(predic_acc[0][1]) != 100:
         print(prod, "not succesful")
         if predic_acc[1]!=None:
